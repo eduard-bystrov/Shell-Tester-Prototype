9473c7c50eee8f44df3e2c84ba9afb001e6f9bb1
eduard-bystrov <eduard.bystr0v@yandex.ru>
2017-12-01 12:37:00 +0300

    test commit

---
+++ ShellTester/ShellTester.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShellTester
+{
+    public class ShellTester : IShellTester
+    {
+        public void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
