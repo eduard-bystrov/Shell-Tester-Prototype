59bfb58106b8e2675b08cef34034854404d0b025
eduard-bystrov <eduard.bystr0v@yandex.ru>
2018-05-15 12:19:21 +0300

    init Postman

---
+++ Postman/Helpers/IEnumerableExtension.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Postman.Helpers
+{
+	public static class IEnumerableExtension<T>
+	{
+		public static String CreateHtmlTable(IEnumerable<T> list, params Func<T, Object>[] fxns)
+		{
+
+			StringBuilder sb = new StringBuilder();
+			sb.Append("<TABLE>\n");
+			foreach (var item in list)
+			{
+				sb.Append("<TR>\n");
+				foreach (var fxn in fxns)
+				{
+					sb.Append("<TD>");
+					sb.Append(fxn(item));
+					sb.Append("</TD>");
+				}
+				sb.Append("</TR>\n");
+			}
+			sb.Append("</TABLE>");
+
+			return sb.ToString();
+		}
+	}
+}
